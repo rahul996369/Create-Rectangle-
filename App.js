dc097d4a64d620a661d6667912679c45422536ad
rahul996369 <103472845+rahul996369@users.noreply.github.com>
2023-02-10 16:58:44 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import Rectangle from "./Rectangle";
+
+import "./styles.css";
+import ParkingSlots from "./ParkingSlots";
+
+export default function App() {
+  return (
+    <div className="App">
+     
+      <Rectangle/>
+      <ParkingSlots/>
+
+    </div>
+  );
+}
